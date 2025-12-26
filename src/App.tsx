7962f6637e23dec11a5cebe9586e3d64737b9647
Bohdan Diatliuk <dyatlyuk.bohdan@gmail.com>
2025-12-27 01:12:50 +0200

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
 import { AppRouter } from "./routes/AppRouter";
+import './App.scss';
+import white from './assets/bg-white.mp4';
+import { ScrollTop } from "./components/ScrollTop";
 
 function App() {
-  return <AppRouter />;
+  return (
+    <>
+      <div className="video-bg">
+        <video autoPlay muted loop playsInline>
+          <source src={white} type="video/mp4" />
+        </video>
+      </div>
+      <div className="content">
+        <ScrollTop />
+        <AppRouter />
+      </div>
+    </>
+  );
 }
 
 export default App;
--- src/components/UserCard/UserCard.tsx
+++ src/components/UserCard/UserCard.tsx
@@ -23,39 +23,47 @@ export const UserCard: React.FC<User> = ({
         <h2 className="card__name">{name}</h2>
 
         <div className="card__info">
-          <div className="card__info-item">Gender: {gender}</div>
           <div className="card__info-item">
-            Location: {city}, {country}
+            <span className="card__info-item-first">Gender:</span>
+            <span className="card__info-item-second">{gender}</span>
           </div>
-          <div className="card__info-item">Email: {email}</div>
+          <div className="card__info-item">
+            <span className="card__info-item-first">Location:</span>
+            <span className="card__info-item-second">
+              {city}, {country}
+            </span>
+          </div>
+          <div className="card__info-item">
+            <span className="card__info-item-first">Email:</span>
+            <span className="card__info-item-second">{email}</span>
+          </div>
         </div>
 
         {weather && (
           <div className="card__weather">
             <h3 className="card__weather-title">Weather</h3>
             <div className="card__weather-block">
-            <span className="card__weather-icon">
-              {WeatherIcons(weather.current.weatherCode, weather.current.isDay)}
-            </span>
-            <span className="card__weather-temp">
-               {weather.current.temperature}°C
-            </span>
-            <span className="card__weather-low">
-              <span className="card__weather-low-icon">
-                <MoveDown size={13} />
+              <span className="card__weather-icon">
+                {WeatherIcons(
+                  weather.current.weatherCode,
+                  weather.current.isDay
+                )}
               </span>
-              <span>
-               {weather.current.lowTemp}°C
+              <span className="card__weather-temp">
+                {weather.current.temperature}°C
               </span>
-            </span>
-            <span className="card__weather-high">
-              <span className="card__weather-high-icon">
-                <MoveUp size={13} />
+              <span className="card__weather-low">
+                <span className="card__weather-low-icon">
+                  <MoveDown size={13} />
+                </span>
+                <span>{weather.current.lowTemp}°C</span>
               </span>
-              <span>
-               {weather.current.highTemp}°C
+              <span className="card__weather-high">
+                <span className="card__weather-high-icon">
+                  <MoveUp size={13} />
+                </span>
+                <span>{weather.current.highTemp}°C</span>
               </span>
-            </span>
             </div>
           </div>
         )}
