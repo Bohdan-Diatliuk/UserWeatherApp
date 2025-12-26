c85f945d017df2c811bac913f686ccaff0a4fcd9
Bohdan Diatliuk <dyatlyuk.bohdan@gmail.com>
2025-12-27 01:47:21 +0200

    fix some details and add variables

--- src/api/types.ts
+++ src/api/types.ts
@@ -1,12 +1,10 @@
 export interface WeatherApiResponse {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    daily: any;
     current_weather: {
         is_day: number;
         temperature: number;
         weathercode: number;
     };
-    forecast: {
+    daily: {
         time: string[];
         temperature_2m_min: number[];
         temperature_2m_max: number[];
--- src/api/weatherApi.ts
+++ src/api/weatherApi.ts
@@ -1,27 +1,13 @@
 import axios from "axios";
 import type { Weather } from "../types/weather";
+import { mapWeather } from "../utils/weatherMap";
 
 export const fetchWeatherData = async (latitude: string, longitude: string): Promise<Weather> => {
   try {
     const response = await axios.get(
       `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m,weather_code,is_day&daily=temperature_2m_max,temperature_2m_min,weather_code&timezone=auto&forecast_days=7`
     );
-    
-    return {
-      current: {
-        temperature: response.data.current.temperature_2m,
-        weatherCode: response.data.current.weather_code,
-        isDay: response.data.current.is_day,
-        lowTemp: response.data.daily.temperature_2m_min[0],
-        highTemp: response.data.daily.temperature_2m_max[0],
-      },
-      forecast: response.data.daily.time.map((date: string, index: number) => ({
-        date,
-        lowTemp: response.data.daily.temperature_2m_min[index],
-        highTemp: response.data.daily.temperature_2m_max[index],
-        weatherCode: response.data.daily.weather_code[index],
-      }))
-    };
+    return mapWeather(response.data);
   } catch (error) {
     console.error("Error fetching weather data:", error);
     throw error;
